3193fd1defb74bbcf0a1368cb962bb45e086d79c
Balqisramadhan <balqisr437@gmail.com>
2023-07-16 17:44:51 +0700

    first commit

---
+++ gobelanja/app/routes.go
@@ -0,0 +1,11 @@
+package app
+
+import (
+	"github.com/Balqisramadhan/gobelanja/app/controllers"
+	"github.com/gorilla/mux"
+)
+
+func (server *Server) InitializeRoutes() {
+	server.Router = mux.NewRouter()
+	server.Router.HandleFunc("/", controllers.Home).Methods("GET")
+}
---
+++ gobelanja/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/Balqisramadhan/gobelanja/app"
+
+func main() {
+	app.Run()
+}
